e665c8969c3a2a4feb7b06c2b7194a8df3793100
Nishant Bhujwan <nishant.app.testing@gmail.com>
2025-04-05 00:45:57 +0530

    pwa files changes

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,38 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker installing.');
-    self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Service Worker activating.');
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
-  });
-  
+const CACHE_NAME = "store-cache-v1";
+const urlsToCache = [
+    "/dsa-in-php/",
+    "/dsa-in-php/index.html",
+    "/dsa-in-php/manifest.json",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
